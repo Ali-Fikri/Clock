14f26385c59d3f59110e111e322b82a69e34d866
Ali-Fikri <alifikripr@gmail.com>
2023-04-04 14:39:49 +0200

    add JS code

---
+++ main.js
@@ -0,0 +1,16 @@
+const secondCircle = document.getElementById("second");
+const minuteCircle = document.getElementById("minute");
+const hourCircle = document.getElementById("hour");
+
+setInterval(setClock, 1000);
+
+function setClock() {
+  const now = new Date();
+  const seconds = now.getSeconds() * 6;
+  const minutes = now.getMinutes() * 6;
+  const hour = now.getHours() * 30;
+
+  hourCircle.style.transform = `rotateZ(${hour + (minutes / 12)}deg)`;
+  minuteCircle.style.transform = `rotateZ(${minuteCircle}deg)`;
+  secondCircle.style.transform = `rotateZ(${seconds}deg)`;
+};
